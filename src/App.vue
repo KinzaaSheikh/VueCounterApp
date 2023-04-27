dd353f721fd8068246fc145c4b4886976655945c
Kinza Sheikh <75093626+KinzaaSheikh@users.noreply.github.com>
2023-04-27 21:50:04 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import {ref} from "vue"
+  const count = ref(0);
+</script>
+
+<template>
+   <main>
+    <div>
+    <h4>
+      The current count is...
+    </h4>
+    <h1>{{count}}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+   </div>
+   </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: azure;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div {
+    text-align: center;
+  }
+
+  button {
+    width: 5rem;
+    margin: 1rem;
+    background-color: brown;
+  }
+</style>
